19089967b2775d1ee3789b7773bbb15cb3fff55f
OS X <lyfirst@naver.com>
2017-07-24 10:47:28 +0900

    chartjs failed

--- app/controllers/lognotes_controller.rb
+++ app/controllers/lognotes_controller.rb
@@ -16,6 +16,27 @@ class LognotesController < ApplicationController
     redirect_to root_url, notice: 'lognotes imported.'
   end
 
+  # def show
+  #   @lognote = Lognote.find(params[:id])
+  #
+  #   colors = ['rgba(255, 99, 132, 0.2)',
+  #         'rgba(54, 162, 235, 0.2)',
+  #         'rgba(255, 206, 86, 0.2)',
+  #         'rgba(75, 192, 192, 0.2)',
+  #         'rgba(153, 102, 255, 0.2)',
+  #         'rgba(255, 159, 64, 0.2)'
+  #         ]
+  #
+  #   @data1 = {
+  #       labels: "Red"
+  #       datasets: [{
+  #         label: "# of Registrations",
+  #         data: @lognote.title.count,
+  #         backgroundColor: colors,
+  #         borderWidth: 1
+  #         }]
+  #   }
+  # end
 
 
 
