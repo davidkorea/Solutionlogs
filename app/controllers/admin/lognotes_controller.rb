d9eb8c999e4834a63f09e921e70ac114de5a9051
OS X <lyfirst@naver.com>
2017-07-25 20:54:38 +0900

    font-awesome-rails

--- app/controllers/admin/lognotes_controller.rb
+++ app/controllers/admin/lognotes_controller.rb
@@ -47,13 +47,6 @@ class Admin::LognotesController < ApplicationController
   end
 
 
-  def require_is_admin
-    if !current_user.admin?
-      flash[:alert] = "You are not admin"
-      redirect_to root_path
-    end
-  end
-
 
   private
 
